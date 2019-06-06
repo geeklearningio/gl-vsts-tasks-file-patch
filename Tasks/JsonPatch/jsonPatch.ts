73d4b29506f3dbd0cbccfdf87b558a350147bd45
Cyprien Autexier <2660262+sandorfr@users.noreply.github.com>
2019-06-06 22:00:55 +1000

    output stack

--- Tasks/JsonPatch/jsonPatch.ts
+++ Tasks/JsonPatch/jsonPatch.ts
@@ -1,17 +1,17 @@
-import path = require('path');
-import fs = require('fs-extra');
-import tl = require('azure-pipelines-task-lib/task');
-import micromatch = require('micromatch');
+import path = require("path");
+import fs = require("fs-extra");
+import tl = require("azure-pipelines-task-lib/task");
+import micromatch = require("micromatch");
 
-import patchProcess = require('./common/patchProcess');
-import jsonPatcher = require('./common/jsonPatcher');
-import json5Patcher = require('./json5Patcher');
-import { Operation } from 'fast-json-patch';
+import patchProcess = require("./common/patchProcess");
+import jsonPatcher = require("./common/jsonPatcher");
+import json5Patcher = require("./json5Patcher");
+import { Operation } from "fast-json-patch";
 
 var targetPath = tl.getPathInput("JsonWorkingDir");
 var patchContent = tl.getInput("JsonPatchContent");
 
-var patterns: any = tl.getInput("JsonTargetFilters")
+var patterns: any = tl.getInput("JsonTargetFilters");
 var outputPatchedFile = tl.getBoolInput("OutputPatchFile");
 var failIfNoPatchApplied = tl.getBoolInput("FailIfNoPatchApplied");
 var treatErrors = tl.getInput("TreatErrors");
@@ -20,15 +20,25 @@ var useJson5 = tl.getBoolInput("UseJson5");
 var produceJson5 = tl.getBoolInput("ProduceJson5");
 
 try {
-    var patches: Operation[] = syntax == "slick" ? 
-    patchProcess.expandVariablesAndParseSlickPatch(patchContent) :
-    patchProcess.expandVariablesAndParseJson(patchContent);
+  var patches: Operation[] =
+    syntax == "slick"
+      ? patchProcess.expandVariablesAndParseSlickPatch(patchContent)
+      : patchProcess.expandVariablesAndParseJson(patchContent);
 
-    var patcher = useJson5 ? new json5Patcher.Json5Patcher(patches, produceJson5) : new jsonPatcher.JsonPatcher(patches);
+  var patcher = useJson5
+    ? new json5Patcher.Json5Patcher(patches, produceJson5)
+    : new jsonPatcher.JsonPatcher(patches);
 
-    patchProcess.apply(patcher, targetPath, patterns, outputPatchedFile, failIfNoPatchApplied, treatErrors);
-
+  patchProcess.apply(
+    patcher,
+    targetPath,
+    patterns,
+    outputPatchedFile,
+    failIfNoPatchApplied,
+    treatErrors
+  );
 } catch (err) {
-    console.error(String(err));
-    tl.setResult(tl.TaskResult.Failed, String(err));
+  console.error(String(err));
+  console.error((err as Error).stack);
+  tl.setResult(tl.TaskResult.Failed, String(err));
 }
